c924f60318ca3c92f173ade45f411a1f0eebce2e
Oksana Kovalchuk <oksana@anoda.mobi>
2014-11-23 18:41:10 +0200

    add directions, and top pins

--- ANDrawerNC.m
+++ ANDrawerNC.m
@@ -7,33 +7,43 @@
 
 #import "ANDrawerNC.h"
 #import "MSSPopMasonry.h"
+
 #define MCANIMATE_SHORTHAND
 #import <POP+MCAnimate.h>
 
 static CGFloat const kDefaultDrawerVelocityTrigger = 350;
+static CGFloat const kStatusBarHeight = 20;
 
-static CGFloat CDCalculateStatusBarHeight() {
-    return (UIDeviceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)) ?
-    [[UIApplication sharedApplication] statusBarFrame].size.width :
-    [[UIApplication sharedApplication] statusBarFrame].size.height;
-}
 
 @interface ANDrawerNC () <UIGestureRecognizerDelegate>
 
 @property (nonatomic, assign) BOOL isOpen;
-@property (nonatomic, strong) UIView *shawdowView;
-@property (nonatomic, strong) MASConstraint* leftSideConstraint;
+@property (nonatomic, strong) UIView *backgroundView;
+@property (nonatomic, strong) MASConstraint* animatedConstraint;
+@property (nonatomic, strong) MASConstraint* topConstraint;
 
 @property (nonatomic, strong) UIView *drawerView;
 @property (nonatomic, assign) CGFloat drawerWidth;
+@property (nonatomic, assign) ANDrawerOpenDirection openDirection;
+@property (nonatomic, strong) UIPanGestureRecognizer* panGesure;
 
 @end
 
 @implementation ANDrawerNC
 
-- (void)updateDrawerView:(UIView *)drawerView width:(CGFloat)drawerWidth
++ (instancetype)drawerWithView:(UIView*)view width:(CGFloat)width direction:(ANDrawerOpenDirection)direction
+{
+    ANDrawerNC* instance = [ANDrawerNC new];
+    [instance setupDrawerView:view width:width openDirection:direction];
+    return instance;
+}
+
+#pragma mark - Setters/Getters
+
+- (void)setupDrawerView:(UIView *)drawerView width:(CGFloat)drawerWidth openDirection:(ANDrawerOpenDirection)direction
 {
     self.drawerWidth = drawerWidth;
+    self.openDirection = direction;
     self.drawerView = drawerView;
 }
 
@@ -46,86 +56,148 @@ static CGFloat CDCalculateStatusBarHeight() {
     _drawerView = drawerView;
     _drawerView.alpha = 0;
     
-    [self.view bringSubviewToFront:self.shawdowView];
     [self.view addSubview:_drawerView];
     [self.view bringSubviewToFront:self.navigationBar];
     
+    [_drawerView addGestureRecognizer:self.panGesure];
+    
     [_drawerView mas_makeConstraints:^(MASConstraintMaker *make) {
 
-        make.top.bottom.equalTo(self.view);
-        make.top.offset(CDCalculateStatusBarHeight());
+        make.bottom.equalTo(self.view);
         make.width.equalTo(@(self.drawerWidth));
-        self.leftSideConstraint = make.left.equalTo(self.view.mas_right).offset(0);
+        
+        self.topConstraint = make.top.equalTo(self.view).offset([self _topOffsetFromPinType:self.topPin]);
+        
+        if (self.openDirection == ANDrawerOpenDirectionFromLeft)
+        {
+            self.animatedConstraint = make.right.equalTo(self.view.mas_left).offset(0);
+        }
+        else
+        {
+            self.animatedConstraint = make.left.equalTo(self.view.mas_right).offset(0);
+        }
     }];
 }
 
-- (UIView *)shawdowView
+- (void)setBackgroundColor:(UIColor *)backgroundColor
 {
-    if (!_shawdowView)
+    _backgroundColor = backgroundColor;
+    self.useBackground = YES;
+    self.backgroundView.backgroundColor = backgroundColor;
+}
+
+- (CGFloat)_topOffsetFromPinType:(ANDrawerTopPin)pinType
+{
+    CGFloat topOffset = 0;
+    if (pinType == ANDrawerTopPinStatusBar)
     {
-        _shawdowView = [UIView new];
-        _shawdowView.hidden = YES;
-        [self.view addSubview:_shawdowView];
+        topOffset = kStatusBarHeight;
+    }
+    else if (pinType == ANDrawerTopPinNavigationBar)
+    {
+        CGFloat navigationBarHeight = self.navigationBarHidden ? 0 : self.navigationBar.height;
+        topOffset = kStatusBarHeight + navigationBarHeight;
+    }
+    return topOffset;
+}
+
+- (void)setTopPin:(ANDrawerTopPin)topPin
+{
+    _topPin = topPin;
+    if (self.topConstraint)
+    {
+        self.topConstraint.offset([self _topOffsetFromPinType:topPin]);
+        [self.view layoutIfNeeded];
+    }
+}
+
+- (void)setUseBackground:(BOOL)useBackground
+{
+    _useBackground = useBackground;
+    if (_backgroundView && !useBackground)
+    {
+        [_backgroundView removeFromSuperview];
+    }
+    [self backgroundColor];
+}
+
+- (UIView *)backgroundView
+{
+    if (!_backgroundView && self.useBackground)
+    {
+        _backgroundView = [UIView new];
+        _backgroundView.hidden = YES;
+        [self.view insertSubview:_backgroundView belowSubview:self.drawerView];
         
-        [_shawdowView mas_makeConstraints:^(MASConstraintMaker *make) {
+        [_backgroundView mas_makeConstraints:^(MASConstraintMaker *make) {
             make.edges.equalTo(self.view);
         }];
+        
+        [_backgroundView addGestureRecognizer:self.panGesure];
+        
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] init];
+        [[tap.rac_gestureSignal deliverOn:[RACScheduler mainThreadScheduler]] subscribeNext:^(id x) {
+            [self updateStateToOpened:NO];
+        }];
+        [_backgroundView addGestureRecognizer:tap];
     }
-    return _shawdowView;
+    return _backgroundView;
 }
 
-- (void)viewDidLoad
+- (UIPanGestureRecognizer *)panGesure
 {
-    [super viewDidLoad];
-    
-    UITapGestureRecognizer *tapIt = [[UITapGestureRecognizer alloc] init];
-    [[tapIt.rac_gestureSignal deliverOn:[RACScheduler mainThreadScheduler]] subscribeNext:^(id x) {
-        [self updateDrawerStateToOpened:NO];
-    }];
-    
-    UIPanGestureRecognizer* pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveDrawer:)];
-    [self.drawerView addGestureRecognizer:pan];
-    
-    [self.shawdowView addGestureRecognizer:tapIt];
-    [self.shawdowView addGestureRecognizer:pan];
+    if (!_panGesure)
+    {
+        _panGesure = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(_moveDrawer:)];
+    }
+    return _panGesure;
 }
+
+#pragma mark - Global Opening State
 
 - (void)toggle
 {
-    [self updateDrawerStateToOpened:!self.isOpen];
+    [self updateStateToOpened:!self.isOpen];
 }
 
-#pragma mark - open and close action
-
-- (void)updateDrawerStateToOpened:(BOOL)isOpen
+- (void)updateStateToOpened:(BOOL)isOpen
 {
     if (isOpen)
     {
-        self.shawdowView.hidden = NO;
-        [self.view endEditing:YES]; // hack for keyboard;
+        self.backgroundView.hidden = NO;
+        if (self.avoidKeyboard)
+        {
+            [self.view endEditing:YES]; // hack for keyboard;
+        }
     }
     
-    CGFloat newOffset = isOpen ? -self.drawerWidth : 0;
+    CGFloat newOffset = isOpen ? [self _offsetForOpenState] : 0;
+    
     POPSpringAnimation *leftSideAnimation = [POPSpringAnimation new];
     leftSideAnimation.toValue = @(newOffset);
     leftSideAnimation.property = [POPAnimatableProperty mas_offsetProperty];
     leftSideAnimation.springBounciness = 4;
-    [self.leftSideConstraint pop_addAnimation:leftSideAnimation forKey:@"offset"];
+    [self.animatedConstraint pop_addAnimation:leftSideAnimation forKey:@"offset"];
     
     self.drawerView.spring.alpha = isOpen;
     
     [NSObject animate:^{
-        self.shawdowView.alpha = isOpen;
+        self.backgroundView.spring.alpha = isOpen;
     } completion:^(BOOL finished) {
-        if (!isOpen) self.shawdowView.hidden = YES;
+        if (!isOpen) self.backgroundView.hidden = YES;
     }];
     
     self.isOpen = isOpen;
 }
 
-#pragma mark - gestures
+#pragma mark - Private
 
-- (void)moveDrawer:(UIPanGestureRecognizer *)recognizer
+- (CGFloat)_offsetForOpenState
+{
+    return (self.openDirection == ANDrawerOpenDirectionFromLeft) ? self.drawerWidth : -self.drawerWidth;
+}
+
+- (void)_moveDrawer:(UIPanGestureRecognizer *)recognizer
 {
     [self.view endEditing:YES]; // hack for keyboard;
     CGPoint translation = [recognizer translationInView:self.view];
@@ -135,26 +207,43 @@ static CGFloat CDCalculateStatusBarHeight() {
     {
         if (velocity.x > kDefaultDrawerVelocityTrigger && !self.isOpen)
         {
-            [self updateDrawerStateToOpened:YES];
+            [self updateStateToOpened:YES];
         }
         else if (velocity.x < -kDefaultDrawerVelocityTrigger && self.isOpen)
         {
-            [self updateDrawerStateToOpened:NO];
+            [self updateStateToOpened:NO];
         }
     }
     else if (recognizer.state == UIGestureRecognizerStateChanged)
     {
-        CGFloat startOffset = self.isOpen ? -self.drawerWidth : 0;
+        CGFloat startOffset = self.isOpen ? [self _offsetForOpenState] : 0;
         CGFloat newOffset = startOffset + translation.x;
         
-        newOffset = MIN(0, MAX(-self.drawerWidth, newOffset));
-        self.leftSideConstraint.offset(newOffset);
-        self.shawdowView.hidden = NO;
+        if (self.openDirection == ANDrawerOpenDirectionFromLeft)
+        {
+            newOffset = MAX(0, MIN([self _offsetForOpenState], newOffset));
+        }
+        else
+        {
+            newOffset = MIN(0, MAX([self _offsetForOpenState], newOffset));
+        }
+        
+        self.animatedConstraint.offset(newOffset);
+        self.backgroundView.hidden = NO;
+        self.backgroundView.alpha = 1 / self.drawerWidth * newOffset + 0.5;
     }
     else if (recognizer.state == UIGestureRecognizerStateEnded)
     {
-        BOOL newState = (self.drawerView.center.x < self.view.width);
-        [self updateDrawerStateToOpened:newState];
+        BOOL newState;
+        if (self.openDirection == ANDrawerOpenDirectionFromLeft)
+        {
+            newState = (self.drawerView.center.x > (self.drawerWidth / 2));
+        }
+        else
+        {
+            newState = (self.drawerView.center.x < (self.drawerWidth / 2));
+        }
+        [self updateStateToOpened:newState];
     }
 }
 
